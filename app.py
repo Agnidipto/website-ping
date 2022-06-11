1345f4e06de7f594920ed5be0f2728af3ded5225
unknown <agnidipto12.25@gmail.com>
2022-06-11 10:44:57 +0530

    Ping more websites"

--- app.py
+++ app.py
@@ -4,5 +4,7 @@ import requests
 a = requests.get('http://dewalisinha.com')
 b = requests.get('https://house-price-react.herokuapp.com')
 c = requests.get('https://apartment-backend-flask.herokuapp.com/get_location_names')
+d = requests.get('https://staying-together.herokuapp.com')
+e = requests.get('https://dewali-sinha.herokuapp.com/test/users')
 
 print(b)
